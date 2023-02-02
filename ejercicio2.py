8f34b97654cadc263a581d373a9876ca4752bd07
Cecia Varela <cvarelar@fpcoruna.afundacion.org>
2023-02-03 00:11:29 +0100

    Ejercicios hasta apartado 5

---
+++ ejercicio2.py
@@ -0,0 +1,22 @@
+lado = input("Ingrese la medida del lado del cuadrado: ")
+lado = int(lado)
+superficie = lado * lado
+print("La superficie del cuadrado es")
+print(superficie)
+
+
+""" 
+lado=int(input("Ingrese la medida del lado del cuadrado:"))
+superficie = lado * lado
+print("La superficie del cuadrado es")
+print(Superficie)
+"""
+
+
+"""
+lado = input("Ingrese la medida del lado del cuadrado: ")
+lado = int(lado)
+superficie = lado * lado * lado
+print("La superficie del cuadrado es")
+print(superficie)
+"""
---
+++ ejercicio8.py
@@ -0,0 +1,5 @@
+horas_trabajas = int(input("Ingrese horas trabajadas: "))
+valor_hora = int(input("Ingrese el valor por hora: "))
+sueldo = horas_trabajas * valor_hora
+print("El sueldo es de")
+print(sueldo)
